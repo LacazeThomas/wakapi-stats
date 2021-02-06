6468f35bc3f48527832fc7777cfca636cd77197d
Thomas LACAZE <contact@thomaslacaze.fr>
2021-02-06 19:45:47 +0100

    add unit on generating image

--- utils/chat_test.go
+++ utils/chat_test.go
@@ -1,1 +1,30 @@
 package utils
+
+import (
+	"testing"
+
+	"github.com/lacazethomas/wakapi-stats/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateStatsDiagram(t *testing.T) {
+	summaries := models.Summary{
+		UserID: "thomaslacaze",
+		From:   struct{}{},
+		To:     struct{}{},
+		Projects: []models.SummaryItem{
+			{
+				Key:   "space",
+				Total: 73012,
+			},
+		},
+		Languages:        []models.SummaryItem{},
+		Editors:          []models.SummaryItem{},
+		OperatingSystems: []models.SummaryItem{},
+		Machines:         []models.SummaryItem{},
+	}
+
+	err := CreateStatsDiagram(summaries.Projects, "projects.png")
+	assert.ErrorIs(t, nil, err)
+	assert.FileExists(t, "projects.png")
+}
